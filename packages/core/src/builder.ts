8ff527d5e81886c788384d6f1c838344c76da020
Kaciras <Kaciras@protonmail.com>
2023-03-30 18:08:13 +0800

    preprocess params

--- packages/core/src/builder.ts
+++ packages/core/src/builder.ts
@@ -1,11 +1,29 @@
-import { NOOP } from "./utils.js";
+import { noop } from "@kaciras/utilities/browser";
+
+export type BenchmarkFn = () => any;
+
+export interface SuiteOptions {
+	params?: ParamsConfig;
+	time?: number;
+	iterations?: number | string;
+}
+
+export type MainFn = (suite: BenchmarkContext, params: ConfigData) => void;
+
+export type ConfigData = Record<string, any>;
+
+export interface BenchmarkCase {
+	name: string;
+	async: boolean;
+	fn: BenchmarkFn;
+}
 
 export class BenchmarkContext {
 
 	readonly benchmarks: BenchmarkCase[] = [];
 
-	setupHook: () => any = NOOP;
-	teardownHook: () => any = NOOP;
+	setupHook: () => any = noop;
+	teardownHook: () => any = noop;
 
 	beforeEach(fn: () => any) {
 		this.setupHook = fn;
@@ -26,7 +44,6 @@ export class BenchmarkContext {
 
 export type ParamsConfig = Record<string, any[]>;
 
-
-export function defineBenchmark(options: SuiteOptions, build: MainFn) {
-	return { options, build };
+export function defineBenchmark(options: SuiteOptions, mainFn: MainFn) {
+	return { options, mainFn };
 }
--- packages/core/src/core.ts
+++ packages/core/src/core.ts
@@ -1,11 +1,16 @@
-import parseDuration from "parse-duration";
-import { cartesianProductObj } from "@kaciras/utilities/browser";
-import { BenchmarkContext, ParamsConfig } from "./builder.js";
+import { Awaitable, cartesianProductObj, durationConvertor } from "@kaciras/utilities/browser";
+import { BenchmarkCase, BenchmarkContext, BenchmarkFn, MainFn, SuiteOptions } from "./builder.js";
 
 export enum MessageType {
-	Turn,
+	Suite,
 	Case,
+	Turn,
 	Finished,
+}
+
+export interface SuiteMessage {
+	type: MessageType.Suite;
+	paramDefs: Record<string, any[]>;
 }
 
 export interface CaseMessage {
@@ -19,9 +24,11 @@ export interface TurnMessage {
 	metrics: Record<string, any>;
 }
 
+// =========================================================================
+
 export type Channel = (message: any) => void;
 
-type IterateFn = (count: number) => Promise<number>;
+type IterateFn = (count: number) => Awaitable<number>;
 
 function runSync(fn: BenchmarkFn, count: number) {
 	const start = performance.now();
@@ -51,23 +58,6 @@ async function getIterations(fn: IterateFn, targetMS: number) {
 	return Math.ceil(count / 2 * targetMS / time);
 }
 
-export interface SuiteOptions {
-	params?: ParamsConfig;
-	time?: number;
-	iterations?: number | string;
-}
-
-type MainFn = (suite: BenchmarkContext, params: ConfigData) => void;
-
-export type ConfigData = Record<string, any>;
-
-export type BenchmarkFn = () => any;
-
-export interface BenchmarkCase {
-	name: string;
-	async: boolean;
-	fn: BenchmarkFn;
-}
 
 export class BenchmarkSuite {
 
@@ -105,11 +95,14 @@ export class BenchmarkSuite {
 		const { time = 5, iterations = 10_000 } = this.options;
 		const { name, fn, async } = case_;
 
-		const runFn = (async ? runAsync : runSync).bind(null, fn);
+		const runFn: IterateFn = async
+			? runAsync.bind(null, fn)
+			: runSync.bind(null, fn);
 
+		// noinspection SuspiciousTypeOfGuard (false positive)
 		const count = typeof iterations === "number"
 			? iterations
-			: await getIterations(runFn, parseDuration(iterations));
+			: await getIterations(runFn, durationConvertor.parse(iterations, "ms"));
 
 		console.log("Count:" + count);
 
@@ -122,16 +115,57 @@ export class BenchmarkSuite {
 	}
 }
 
+function short(value: any, length: number) {
+	if (value.length < length) {
+		return value;
+	}
+	const n = (length - 3) / 2;
+	return `${value.slice(0, Math.ceil(n))}...${value.slice(-Math.floor(n))}`;
+}
+
+export function serializable(params: Record<string, Iterable<unknown>>) {
+	const entries = Object.entries(params);
+	const processed: Record<string, any> = {};
+	const counters = new Array(entries.length).fill(0);
+
+	let current: any[];
+
+	for (let i = 0; i < entries.length; i++) {
+		const [key, values] = entries[i];
+		processed[key] = current = [];
+
+		for (const v of values) {
+			const k = counters[i]++;
+			switch (typeof v) {
+				case "object":
+					current.push(v === null ? "null" : `object #${k}`);
+					break;
+				case "symbol":
+					current.push(v.description
+						? `symbol(${short(v.description, 10)}) #${k}`
+						: `symbol #${k}`,
+					);
+					break;
+				case "function":
+					current.push(`func ${short(v.name, 10)} #${k}`);
+					break;
+				default:
+					current.push(short("" + v, 16));
+			}
+		}
+	}
+
+	return processed;
+}
+
 export interface BenchmarkModule {
 	default: MainFn;
 	options: SuiteOptions;
 }
 
-type Suites = Record<string, () => Promise<BenchmarkModule>>;
+export default async function runSuite(mod: Promise<BenchmarkModule>, name: string, channel: Channel) {
+	const { default: { options, mainFn } } = await mod;
+	console.error(options);
 
-export default async function runSuites(suites: Suites, name: string, channel: Channel) {
-	for (const [file, import_] of Object.entries(suites)) {
-		const { options, default: mainFn } = await import_();
-		await new BenchmarkSuite(options, mainFn, channel).bench(name);
-	}
+	await new BenchmarkSuite(options, mainFn, channel).bench(name);
 }
