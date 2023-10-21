304aee590e64a9e5518a5a64f3a62eced9c5e828
Kaciras <Kaciras@protonmail.com>
2023-10-21 15:21:22 +0800

    WIP

--- packages/reporter-html/src/App.svelte
+++ packages/reporter-html/src/App.svelte
@@ -1,47 +1,93 @@
-<script lang="ts">
-import svelteLogo from "./assets/svelte.svg";
-import viteLogo from "/vite.svg";
-import Counter from "./lib/Counter.svelte";
-</script>
+<nav>
+	{#each files as [file, scenes]}
+		<a href={encodeURIComponent(file)}>{file}</a>
+	{/each}
+</nav>
+
+<header>
+Suite: {files[0][0]}
+</header>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+	<canvas bind:this={canvasRef}/>
+	<select>
+		<option>Mean</option>
+		<option>Median</option>
+	</select>
 </main>
 
+<section>
+
+</section>
+
+<script lang="ts">
+import type { ESBenchResult } from "@esbench/core/src/client/collect.js";
+import { BarController, BarElement, CategoryScale, Chart, Colors, Legend, LinearScale, Tooltip } from "chart.js";
+import { onMount } from "svelte";
+
+Chart.register(BarController, Tooltip, Colors, CategoryScale, LinearScale, BarElement, Legend);
+
+function mean(values: number[]) {
+	let sum = 0;
+	for (const value of values) {
+		sum += value;
+	}
+	return sum / values.length;
+}
+
+function meaian(values: number[]) {
+	values.sort();
+	return values[Math.round(values.length / 2)];
+}
+
+const files = Object.entries(window.ESBenchResult as ESBenchResult);
+
+const result = files[0][1];
+
+let foldFn = mean;
+let canvasRef: HTMLCanvasElement;
+
+onMount(() => {
+	new Chart(canvasRef, {
+		type: "bar",
+		data: {
+			labels: result.map(r => r.name),
+			datasets: [{
+				label: "time",
+				data: result.map(r => foldFn(r.metrics.time)),
+			}],
+		},
+		options: {
+			responsive: true,
+			plugins: {
+				legend: {
+					position: "top",
+				},
+			},
+		},
+	});
+});
+</script>
+
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+nav {
+    grid-area: nav;
+}
+
+main {
+    grid-area: main;
+}
+
+header {
+    grid-area: header;
+    padding: 10px;
+}
+
+section {
+    grid-area: params;
+}
+
+a {
+    padding: 10px;
+}
 </style>
--- packages/reporter-html/vite.config.ts
+++ packages/reporter-html/vite.config.ts
@@ -1,6 +1,17 @@
-import { defineConfig } from "vite";
+import { readFileSync } from "fs";
+import { defineConfig, Plugin } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { interpolate } from "./lib/index.js";
+
+const result = JSON.parse(readFileSync("../example/perf.json", "utf8"));
+
+const debugDataPlugin: Plugin = {
+	name: "debug-data",
+	transformIndexHtml(html) {
+		return interpolate(html, result);
+	},
+};
 
 export default defineConfig({
-	plugins: [svelte()],
+	plugins: [svelte(), debugDataPlugin],
 });
