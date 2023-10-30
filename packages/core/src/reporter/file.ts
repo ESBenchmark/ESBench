a968d39b7e2d35c995d1dc125c656fb08c048ead
Kaciras <Kaciras@protonmail.com>
2023-10-30 22:41:11 +0800

    remove CSV reporter

--- packages/core/src/reporter/file.ts
+++ packages/core/src/reporter/file.ts
@@ -1,17 +1,6 @@
-import { createWriteStream, writeFileSync } from "fs";
-import * as csv from "csv-stringify";
+import { writeFileSync } from "fs";
 import { Reporter } from "../config.js";
-import { flatResult } from "../client/collect.js";
 
-type ResultFileFormat = "json" | "csv";
-
-export default function (file?: string, format: ResultFileFormat = "json"): Reporter {
-	if (format === "json") {
-		return result => writeFileSync(file ?? "benchmark.json", JSON.stringify(result));
-	} else if (format === "csv") {
-		return result => csv.stringify(Array.from(flatResult(result)))
-			.pipe(createWriteStream(file ?? "benchmark.csv"));
-	} else {
-		throw new TypeError(`Unknown format ${format}, supported values: (json|csv)`);
-	}
+export default function (file?: string): Reporter {
+	return result => writeFileSync(file ?? "benchmark.json", JSON.stringify(result));
 }
--- packages/reporter-html/vite.config.ts
+++ packages/reporter-html/vite.config.ts
@@ -1,10 +1,10 @@
 import { readFileSync } from "fs";
-import { defineConfig, Plugin } from "vite";
+import { defineConfig, type Plugin } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { viteSingleFile } from "vite-plugin-singlefile";
 import { interpolate } from "./lib/index.js";
 
-const result = JSON.parse(readFileSync("../example/perf.json", "utf8"));
+const result = JSON.parse(readFileSync("../example/benchmark.json", "utf8"));
 
 const debugDataPlugin: Plugin = {
 	name: "debug-data",
@@ -13,6 +13,10 @@ const debugDataPlugin: Plugin = {
 	},
 };
 
-export default defineConfig({
-	plugins: [svelte(), debugDataPlugin, viteSingleFile()],
-});
+export default defineConfig(({ mode }) => ({
+	plugins: [
+		viteSingleFile(),
+		svelte(),
+		mode === "serve" && debugDataPlugin,
+	],
+}));
