5b5982d150b6ee4630a3e9eec2be758e085b8d5c
Kaciras <Kaciras@protonmail.com>
2023-11-01 10:49:22 +0800

    add support of add date & time to the filename for file reporter

--- packages/core/src/reporter/file.ts
+++ packages/core/src/reporter/file.ts
@@ -1,6 +1,27 @@
-import { writeFileSync } from "fs";
+import { mkdirSync, writeFileSync } from "fs";
+import { dirname } from "path";
 import { Reporter } from "../config.js";
 
-export default function (file?: string): Reporter {
-	return result => writeFileSync(file ?? "benchmark.json", JSON.stringify(result));
+/**
+ * Save the result object as a JSON file.
+ *
+ * The name pattern support the following placeholders:
+ * [date]: report date, e.g. "2023-11-01"
+ * [time]: report time, e.g. "20-13-51"
+ *
+ * @param name The pattern to use for naming result file.
+ */
+export default function (name = "result_[date]_[time].json"): Reporter {
+	const zoneOffset = new Date().getTimezoneOffset() * 60 * 1000;
+
+	return result => {
+		const now = new Date(Date.now() - zoneOffset);
+		const [date, time] = now.toISOString().split("T");
+		const file = name
+			.replaceAll("[date]", date)
+			.replaceAll("[time]", time.slice(0, -5).replaceAll(":", "-"));
+
+		mkdirSync(dirname(file), { recursive: true });
+		writeFileSync(file, JSON.stringify(result));
+	};
 }
