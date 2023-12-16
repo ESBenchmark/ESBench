ea7123ff5a9044d6f3bda290b5a1613df3eef501
Kaciras <Kaciras@protonmail.com>
2023-12-16 19:08:29 +0800

    refactor text reporter

--- packages/core/src/reporter/text.ts
+++ packages/core/src/reporter/text.ts
@@ -6,9 +6,81 @@ import { durationFmt } from "@kaciras/utilities/node";
 import { mean, quantileSorted, standardDeviation } from "simple-statistics";
 import { markdownTable } from "markdown-table";
 import stringLength from "string-width";
-import { ESBenchResult, SummaryTableFilter } from "../client/collect.js";
+import { ESBenchResult, FlattedCase, SummaryTableFilter } from "../client/collect.js";
 import { Reporter } from "../config.js";
 import { OutlierMode, TukeyOutlierDetector } from "../client/math.js";
+import { BaselineOptions, Metrics } from "../client/index.js";
+
+interface MetricColumnFactory {
+
+	name: string;
+
+	prepare?(stf: SummaryTableFilter, cases: FlattedCase[]): void;
+
+	build(metrics: Metrics): string;
+}
+
+class BaselineColumn implements MetricColumnFactory {
+
+	readonly name = "ratio";
+
+	private readonly key: string;
+	private readonly value: string;
+
+	private ratio1 = 0;
+
+	constructor(baseline: BaselineOptions) {
+		this.key = baseline.type;
+		this.value = baseline.value;
+	}
+
+	prepare(stf: SummaryTableFilter, cases: FlattedCase[]) {
+		const { key, value } = this;
+		const ratio1Row = cases.find(d => d[key] === value);
+		if (!ratio1Row) {
+			throw new Error(`Baseline (${key}=${value}) does not in the table`);
+		}
+		this.ratio1 = mean(stf.getMetrics(ratio1Row).time);
+	}
+
+	build(metrics: Metrics) {
+		const ratio = mean(metrics.time) / this.ratio1;
+		const text = `${ratio.toFixed(2)}x`;
+		if (ratio === 1) {
+			return text;
+		}
+		return ratio < 1 ? chalk.green(text) : chalk.red(text);
+	}
+}
+
+const meanColumn: MetricColumnFactory = {
+	name: "time",
+	build(metrics: Metrics): string {
+		return fmtTime(mean(metrics.time));
+	},
+};
+
+const stdDevColumn: MetricColumnFactory = {
+	name: "stdDev",
+	build(metrics: Metrics): string {
+		return fmtTime(standardDeviation(metrics.time));
+	},
+};
+
+class PercentileColumn implements MetricColumnFactory {
+
+	readonly p: number;
+	readonly name: string;
+
+	constructor(p: number) {
+		this.p = p / 100;
+		this.name = "p" + p;
+	}
+
+	build(metrics: Metrics) {
+		return fmtTime(quantileSorted(metrics.time, this.p));
+	}
+}
 
 async function print(result: ESBenchResult, options: TextReporterOptions, out: Writable, chalk: ChalkInstance) {
 	const { stdDev = false, percentiles = [], outliers = "upper" } = options;
@@ -17,40 +89,42 @@ async function print(result: ESBenchResult, options: TextReporterOptions, out: W
 
 	for (const [name, stages] of entries) {
 		const stf = new SummaryTableFilter(stages);
-		const header = [...stf.vars.keys()];
+		const { baseline } = stages[0];
 
-		for (let i = 0; i < stf.builtinParams.length; i++) {
-			header[i] = chalk.cyan(header[i]);
+		const metricColumns: MetricColumnFactory[] = [meanColumn];
+		if (stdDev) {
+			metricColumns.push(stdDevColumn);
 		}
+		for (const k of percentiles) {
+			metricColumns.push(new PercentileColumn(k));
+		}
+		if (baseline) {
+			metricColumns.push(new BaselineColumn(baseline));
+		}
+
+		const header = [...stf.vars.keys()];
 		for (let i = stf.builtinParams.length; i < header.length; i++) {
 			header[i] = chalk.magentaBright(header[i]);
 		}
-		header.push("time");
-		if (stdDev) {
-			header.push("stdDev");
-		}
-		for (const k of percentiles) {
-			header.push("p" + k);
+		for (const metricColumn of metricColumns) {
+			header.push(chalk.cyan(metricColumn.name));
 		}
 
 		const table = [header];
-		const hints = [];
+		const hints: string[] = [];
 
-		const { baseline } = stages[0];
 		let groups = [stf.table][Symbol.iterator]();
-
 		if (baseline) {
 			groups = stf.groupBy(baseline.type).values();
-			header.push("ratio");
 		}
 
 		for (const group of groups) {
-			const ratio1Row = group.find(d => d[baseline!.type] === baseline!.value);
-			if (!ratio1Row && baseline) {
-				throw new Error(`Baseline (${baseline.type}=${baseline.value}) does not in the table`);
+			for (const data of group) {
+				removeOutliers(data, stf.getMetrics(data));
 			}
-			const ratio1 = ratio1Row ? stf.getMetrics(ratio1Row) : null;
-
+			for (const metricColumn of metricColumns) {
+				metricColumn.prepare?.(stf, group);
+			}
 			for (const data of group) {
 				const columns: string[] = [];
 				table.push(columns);
@@ -58,32 +132,23 @@ async function print(result: ESBenchResult, options: TextReporterOptions, out: W
 				for (const k of stf.vars.keys()) {
 					columns.push("" + data[k]);
 				}
-
-				const rawTime = stf.getMetrics(data).time;
-				let time = rawTime;
-				if (outliers) {
-					time = new TukeyOutlierDetector(rawTime).filter(rawTime, outliers);
-				}
-
-				if (rawTime.length !== time.length) {
-					const removed = rawTime.length - time.length;
-					hints.push(`${data.Name}: ${removed} outliers were removed.`);
-				}
-				columns.push(fmtTime(mean(time)));
-
-				if (stdDev) {
-					columns.push(fmtTime(standardDeviation(time)));
-				}
-				for (const k of percentiles) {
-					columns.push(fmtTime(quantileSorted(time, k / 100)));
-				}
-
-				if (ratio1Row) {
-					const ratio = mean(rawTime) / mean(ratio1!.time);
-					columns.push(`${ratio.toFixed(2)}x`);
+				const metrics = stf.getMetrics(data);
+				for (const metricColumn of metricColumns) {
+					columns.push(metricColumn.build(metrics));
 				}
 			}
 			table.push(new Array(header.length));
+		}
+
+		function removeOutliers(data: FlattedCase, metrics: Metrics){
+			const rawTime = metrics.time;
+			if (outliers) {
+				metrics.time = new TukeyOutlierDetector(rawTime).filter(rawTime, outliers);
+			}
+			if (rawTime.length !== metrics.time.length) {
+				const removed = rawTime.length - metrics.time.length;
+				hints.push(`${data.Name}: ${removed} outliers were removed.`);
+			}
 		}
 
 		table.pop();
