35fcc10d17d69b4f8f8bf1de7f184e941028ef91
Kaciras <Kaciras@protonmail.com>
2024-03-18 18:19:39 +0800

    optimmize example page

--- docs/playground/SuiteExamples.vue
+++ docs/playground/SuiteExamples.vue
@@ -1,30 +1,33 @@
 <template>
-	<header :class='$style.header'>
+	<section :class='$style.header'>
 		<h1>Try benchmarks in the browser</h1>
-		or
-		<a href='/playground.html'>Create</a>
-	</header>
+		<VPButton href='/playground.html' text='Or Create New'/>
+	</section>
 	<section :class='$style.cards'>
 		<a
 			v-for='(suite, i) of suites'
+			:key='suite.name'
 			:class='$style.suite'
 			:href='"/playground.html?demo=" + i'
 		>
 			{{ suite.name }}
-			<div>
+			<span :class='$style.attrs'>
 				{{ suite.params }} params,
 				{{ suite.cases }} cases
-			</div>
+			</span>
 		</a>
 	</section>
 </template>
 
 <script setup lang="ts">
+import { VPButton } from "vitepress/theme";
 import suites from "./demo-suites.ts";
 </script>
 
 <style module>
 .header {
+	display: flex;
+	justify-content: space-between;
 	margin: 30px 0;
 }
 
@@ -41,5 +44,12 @@ import suites from "./demo-suites.ts";
 
 	color: inherit !important;
 	text-decoration: none !important;
+}
+
+.attrs {
+	display: block;
+	font-size: 0.875em;
+	float: right;
+	color: var(--vp-c-text-2);
 }
 </style>
