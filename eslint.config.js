346b741bb6e71a2cd3f344db95a3a26c4fdb3952
Kaciras <Kaciras@protonmail.com>
2024-04-14 08:48:08 +0800

    switch to ESLint flat config

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import core from "@kaciras/eslint-config-core";
+import typescript from "@kaciras/eslint-config-typescript";
+import vueTs from "@kaciras/eslint-config-vue/typescript.js";
+
+export default [...core, ...typescript, ...vueTs,
+	{
+		ignores: [
+			"docs/.vitepress/{cache,dist}/**",
+			"core/{lib,coverage}/**",
+		],
+	},
+	{
+		rules: {
+			"kaciras/import-group-sort": "warn",
+		},
+	},
+];
