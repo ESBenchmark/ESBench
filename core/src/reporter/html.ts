87bb87bfe2a162392bf1dc77b167b39162a58d24
Kaciras <Kaciras@protonmail.com>
2024-05-16 09:15:40 +0800

    drop open browser support of html reporter

--- core/src/reporter/html.ts
+++ core/src/reporter/html.ts
@@ -1,38 +1,28 @@
 import { fileURLToPath, pathToFileURL } from "url";
 import { dirname, join } from "path";
 import { mkdirSync, readFileSync, writeFileSync } from "fs";
-import openBrowser from "open";
 import { ESBenchResult } from "../connect.js";
-
-export interface HtmlReporterOptions {
-	/**
-	 * The name of the reporter file should be saved to.
-	 *
-	 * @default "reports/benchmark.html"
-	 */
-	file?: string;
-
-	/**
-	 * Automatically open the report in the browser.
-	 *
-	 * @default false
-	 */
-	open?: boolean;
-}
 
 // This file is generated from packages/reporter-html.
 const dist = join(fileURLToPath(import.meta.url), "../index.html");
 
-export default function (options: HtmlReporterOptions = {}) {
-	const { file = "reports/benchmark.html", open } = options;
+/**
+ * Plot the results into an interactive chart.
+ *
+ * @param file filename of the html report, default is "reports/benchmark.html"
+ */
+export default function (file = "reports/benchmark.html") {
 	const template = readFileSync(dist, "utf8");
 
 	function interpolate(html: string, p: string, r: ESBenchResult) {
 		const code = JSON.stringify(r);
-		return html.replace(`<!--ESBench-${p}-->`, `<script>window.${p}=${code}</script>`);
+		return html.replace(
+			`<!--ESBench-${p}-->`,
+			`<script>window.${p}=${code}</script>`,
+		);
 	}
 
-	return async (result: ESBenchResult, prev: ESBenchResult) => {
+	return (result: ESBenchResult, prev?: ESBenchResult) => {
 		let html = interpolate(template, "Result", result);
 		if (prev) {
 			html = interpolate(html, "Previous", prev);
@@ -41,9 +31,6 @@ export default function (options: HtmlReporterOptions = {}) {
 		writeFileSync(file, html);
 
 		const url = pathToFileURL(file).toString();
-		if (open) {
-			await openBrowser(url);
-		}
 		console.info("HTML report can be found at: " + url);
 	};
 }
