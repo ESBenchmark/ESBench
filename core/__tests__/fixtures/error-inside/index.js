bb4864a6c68d8381cddd9f4c6ef52228251a073a
Kaciras <Kaciras@protonmail.com>
2024-05-16 16:22:58 +0800

    fix missing error stack in tests

--- core/__tests__/fixtures/error-inside/index.js
+++ core/__tests__/fixtures/error-inside/index.js
@@ -1,3 +1,4 @@
 export default async function (post, files, pattern) {
-	await post({ e: { name: "Error", message: "Stub Error" } });
+	const { stack } = new Error();
+	await post({ e: { name: "Error", message: "Stub Error", stack } });
 }
