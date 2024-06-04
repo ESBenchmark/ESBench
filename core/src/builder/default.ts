edff52bef067601c3f29f7d8916cebfb0d45513f
Kaciras <Kaciras@protonmail.com>
2024-06-04 13:21:18 +0800

    builder: use string literal for dynamic import

--- core/src/builder/default.ts
+++ core/src/builder/default.ts
@@ -2,24 +2,29 @@ import { writeFileSync } from "fs";
 import { join, relative } from "path";
 import { Builder } from "../host/toolchain.js";
 
-const template = `\
-import { runAndSend } from "esbench";
-
-const doImport = file => import("__ROOT__" + file.slice(1));
-
-export default function (channel, files, name) {
-	return runAndSend(channel, doImport, files, name);
-}`;
-
 /**
  * Although no code transformation is needed, it is still necessary
  * to create an entry module according to ESBench conventions.
  */
 export default <Builder>{
 	name: "None",
-	build(outDir: string) {
-		const root = relative(outDir, process.cwd());
-		const code = template.replace("__ROOT__", root.replaceAll("\\", "/"));
+	build(outDir: string, files: string[]) {
+		const root = relative(outDir, process.cwd()).replaceAll("\\", "/");
+		let imports = "";
+		for (const file of files) {
+			imports += `\n\t"${file}":()=>import("${root + file.slice(1)}"),`;
+		}
+		const code = `\
+			import { runAndSend } from "esbench";
+			
+			const suites = {${imports}\n};
+			
+			const doImport = file => suites[file]();
+			
+			export default function (channel, files, pattern) {
+				return runAndSend(channel, doImport, files, pattern);
+			}
+		`;
 		writeFileSync(join(outDir, "index.js"), code);
 	},
 };
