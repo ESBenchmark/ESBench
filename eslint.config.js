2f865eb09a2ed298eaa4fc3fa28fcb85c3f30ec4
Kaciras <contact@kaciras.com>
2025-01-31 19:55:26 +0800

    fix ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import core from "@kaciras/eslint-config-core";
 import typescript from "@kaciras/eslint-config-typescript";
-import vueTs from "@kaciras/eslint-config-vue/typescript.js";
+import vueTs from "@kaciras/eslint-config-vue/typescript";
 
 export default [...core, ...typescript, ...vueTs,
 	{
